9c44ca6cc701398c6ae74d6a50283d8c51f75608
Kevin Cantwell <kevin.cantwell@gmail.com>
2013-06-09 21:05:05 -0400

    Rails init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Jinandjuice::Application.config.secret_token = '61e2c4b7751a321f432371a61f2ef6cd3e58e6bda6fb9b670f0cbe78246fcdf50b81e52c232a1b3553640c59f047a0e7d60d2ca15513094e85505a97a891b0bd'
