a2ae658a6c88c5c847ff0a551fd5c3fb5d6c8589
Kevin Cantwell <kevin.cantwell@gmail.com>
2013-06-12 00:49:25 -0400

    Test styles on video

--- app/models/video.rb
+++ app/models/video.rb
@@ -3,8 +3,9 @@ class Video < ActiveRecord::Base
 
   attr_accessible :attachment
 
-  has_attached_file :attachment
-  # ,
-  #               :url => "#{Rails.root}/app/assets/images/:basename.:extension",
-  #               :path => "#{Rails.root}/app/assets/images/products/:id/:style/:basename.:extension"
+  has_attached_file :attachment, styles: {
+    thumb: '100x100>',
+    square: '200x200#',
+    medium: '300x300>'
+  }
 end
