5573895eb00ea33c751fa88debd51eb929c76f84
Kevin Cantwell <kevin.cantwell@gmail.com>
2013-06-12 01:35:59 -0400

    One more

--- app/models/video.rb
+++ app/models/video.rb
@@ -3,9 +3,5 @@ class Video < ActiveRecord::Base
 
   attr_accessible :attachment
 
-  has_attached_file :attachment, styles: {
-    thumb: '100x100>',
-    square: '200x200#',
-    medium: '300x300>'
-  }
+  has_attached_file :attachment
 end
